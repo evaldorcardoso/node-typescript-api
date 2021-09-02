837e47127dd4491ba26b8eae65b53f76a5418d7e
EvaldoRCardoso <evaldorubel@hotmail.com>
2021-09-02 10:19:22 -0300

    Configurando prettier

--- test/globals.d.ts
+++ test/globals.d.ts
@@ -1,5 +1,6 @@
-declare var testRequest: import('supertest').SuperTest<import('supertest').Test>;
-
+declare var testRequest: import('supertest').SuperTest<
+  import('supertest').Test
+>;
 
 /*declare namespace NodeJS {
     interface Global {
