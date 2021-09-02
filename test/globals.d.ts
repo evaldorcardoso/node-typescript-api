324c71918d510b5b3b61364c0b75a60bd58e8b79
EvaldoRCardoso <evaldorubel@hotmail.com>
2021-09-02 10:06:53 -0300

    Iniciando API com overnight.js e express

---
+++ test/globals.d.ts
@@ -0,0 +1,8 @@
+declare var testRequest: import('supertest').SuperTest<import('supertest').Test>;
+
+
+/*declare namespace NodeJS {
+    interface Global {
+        testRequest: import('supertest').SuperTest<import('supertest').Test>;
+    }
+}*/
