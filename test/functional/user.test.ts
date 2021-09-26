4b73ec634752df50bc69232f64d8cb0dc873245c
Evaldo R. Cardoso <evaldorubel@hotmail.com>
2021-09-25 23:55:18 -0300

    criptografando a senha com bcrypt

--- test/functional/user.test.ts
+++ test/functional/user.test.ts
@@ -1,52 +1,66 @@
-import { User } from "@src/models/user"
+import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 
 describe('Users functional tests', () => {
-
-    beforeEach(async() => {
+    beforeEach(async () => {
         await User.deleteMany({});
-    })
+    });
 
     describe('When creating a new user', () => {
-        it('shluld successfully create a new user', async() => {
+        it('shluld successfully create a new user with encrypted password', async () => {
             const newUser = {
                 name: 'John Doe',
-                email: "john@mail.com",
-                password: "1234"
+                email: 'john@mail.com',
+                password: '1234',
             };
 
-            const response = await global.testRequest.post('/user').send(newUser);
+            const response = await global.testRequest
+                .post('/user')
+                .send(newUser);
             expect(response.status).toBe(201);
-            expect(response.body).toEqual(expect.objectContaining(newUser));
-        })   
-        
+            await expect(
+                AuthService.comparePasswords(newUser.password, response.body.password)
+            ).resolves.toBeTruthy();
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    ...newUser,
+                    ...{ password: expect.any(String) },
+                })
+            );
+        });
+
         it('Should return 422 when there is a validation error', async () => {
             const newUser = {
-              email: 'john@mail.com',
-              password: '1234',
+                email: 'john@mail.com',
+                password: '1234',
             };
-            const response = await global.testRequest.post('/user').send(newUser);
-      
+            const response = await global.testRequest
+                .post('/user')
+                .send(newUser);
+
             expect(response.status).toBe(422);
             expect(response.body).toEqual({
-              code: 422,
-              error: 'User validation failed: name: Path `name` is required.',
+                code: 422,
+                error: 'User validation failed: name: Path `name` is required.',
             });
         });
 
         it('Should return 409 when the email already exists', async () => {
             const newUser = {
-              name: 'John Doe',
-              email: 'john@mail.com',
-              password: '1234',
+                name: 'John Doe',
+                email: 'john@mail.com',
+                password: '1234',
             };
             await global.testRequest.post('/user').send(newUser);
-            const response = await global.testRequest.post('/user').send(newUser);
-      
+            const response = await global.testRequest
+                .post('/user')
+                .send(newUser);
+
             expect(response.status).toBe(409);
             expect(response.body).toEqual({
-              code: 409,
-              error: 'User validation failed: email: already exists in the database.',
+                code: 409,
+                error: 'User validation failed: email: already exists in the database.',
             });
         });
-    })
-})
+    });
+});
